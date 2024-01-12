d1efeaf50bad9ff7225db00a0b459f83d576aceb
george pogosyan <goshansmails@gmail.com>
2024-01-12 22:28:17 +0300

    [iter1] Add echo server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,25 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"net/http"
+)
+
+const addr = "localhost:8080"
+
+func mainHandler(resp http.ResponseWriter, req *http.Request) {
+	resp.Write([]byte("Hello"))
+}
+
+func main() {
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", mainHandler)
+
+	log.Print("start server")
+
+	server := http.Server{Addr: addr, Handler: mux}
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatal(err)
+	}
+}
