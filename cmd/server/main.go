2a3e347c1183530da0346116a62e1a1d08271f0a
george pogosyan <goshansmails@gmail.com>
2024-01-12 23:07:27 +0300

    [iter1] Implement handler

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,12 +3,73 @@ package main
 import (
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
 const addr = "localhost:8080"
 
+type Action string
+
+const (
+	Update Action = "update"
+)
+
+type MetricType string
+
+const (
+	Gauge   MetricType = "gauge"
+	Counter            = "counter"
+)
+
 func mainHandler(resp http.ResponseWriter, req *http.Request) {
-	resp.Write([]byte("Hello"))
+
+	if req.Method != http.MethodPost {
+		resp.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	parts := strings.Split(strings.Trim(req.URL.Path, "/"), "/")
+	if len(parts) != 4 {
+		resp.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	var (
+		action     = Action(parts[0])
+		metricType = MetricType(parts[1])
+		_          = parts[2]
+		value      = parts[3]
+	)
+
+	switch Action(action) {
+	case Update:
+	default:
+		resp.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	switch MetricType(metricType) {
+	case Gauge:
+		_, err := strconv.ParseFloat(value, 64)
+		if err != nil {
+			resp.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+	case Counter:
+		_, err := strconv.ParseInt(value, 10, 64)
+		if err != nil {
+			resp.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+	default:
+		resp.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	resp.WriteHeader(http.StatusOK)
 }
 
 func main() {
